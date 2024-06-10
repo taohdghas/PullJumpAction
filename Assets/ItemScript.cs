36a285409fb0feee92a103fd8e85cc30f62c7144
taohdghas <143993457+taohdghas@users.noreply.github.com>
2024-06-10 10:46:54 +0900

    アイテム作業途中

---
+++ Assets/ItemScript.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        DestroySelf();
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        Debug.Log("Stay");
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Exit");
+    }
+    
+    private void DestroySelf()
+    {
+        Destroy(gameObject);
+    }
+}
