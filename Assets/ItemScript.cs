1041f3988cd0fff45265d3b52eac682c05eef481
taohdghas <143993457+taohdghas@users.noreply.github.com>
2024-06-10 12:09:11 +0900

    サウンド・シーン完了

--- Assets/ItemScript.cs
+++ Assets/ItemScript.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class ItemScript : MonoBehaviour
 {
     private Animator animator;
+    private AudioSource audioSource;
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
+        audioSource = gameObject.GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -20,6 +22,7 @@ public class ItemScript : MonoBehaviour
     {
         // DestroySelf();
         animator.SetTrigger("Get");
+        audioSource.Play();
     }
     private void OnTriggerStay(Collider other)
     {
